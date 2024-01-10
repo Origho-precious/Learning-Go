d1d1600f4dc83a89f6863213c7b1a34315631630
Origho-precious <Origho9@gmail.com>
2024-01-10 12:02:58 +0100

    Fix loop condition in main function

--- ch4/ex1/main.go
+++ ch4/ex1/main.go
@@ -9,7 +9,7 @@ func main() {
 	// var nums []int = []int{}
 	nums := make([]int, 0, 100)
 
-	for i := 0; len(nums) <= 100; i++ {
+	for i := 0; i < 100; i++ {
 		randNum := rand.Intn(100)
 		nums = append(nums, randNum)
 
